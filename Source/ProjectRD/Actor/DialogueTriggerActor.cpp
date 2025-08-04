b12e89adefa2c1005e2316bc0955eb6da5253f2f
woghks-kor <ewoghksqh@gmail.com>
2025-08-04 14:34:29 +0900

    트리거 닿으면 위젯 생성 액터 추가

---
+++ Source/ProjectRD/Actor/DialogueTriggerActor.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/DialogueTriggerActor.h"
+
+#include "Components/BoxComponent.h"
+#include "Blueprint/UserWidget.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+ADialogueTriggerActor::ADialogueTriggerActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	RootComponent = BoxComp;
+}
+
+// Called when the game starts or when spawned
+void ADialogueTriggerActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &ADialogueTriggerActor::OnBoxBeginOverlap);
+	//BoxComp->OnComponentEndOverlap.AddDynamic(this, &ADialogueTriggerActor::OnBoxEndOverlap);
+}
+
+// Called every frame
+void ADialogueTriggerActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ADialogueTriggerActor::OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ACharacter* Player = Cast<ACharacter>(OtherActor);
+	if (!Player) return;
+	
+	if (!WidgetClass) return;
+	WidgetInstance = CreateWidget<UUserWidget>(GetWorld(), WidgetClass);
+
+	if (!WidgetInstance) return;
+	WidgetInstance->AddToViewport();
+
+	if (OverlapSound)
+	{
+		UGameplayStatics::PlaySound2D(this, OverlapSound);
+	}
+
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+	TimerManager.ClearTimer(TimerHandle);
+	TimerManager.SetTimer(TimerHandle, this, &ADialogueTriggerActor::ClearWidget, DisplayTime);
+}
+
+void ADialogueTriggerActor::OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+void ADialogueTriggerActor::ClearWidget()
+{
+	WidgetInstance->RemoveFromParent();
+	Destroy();
+}
---
+++ Source/ProjectRD/Actor/DialogueTriggerActor.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DialogueTriggerActor.generated.h"
+
+UCLASS()
+class PROJECTRD_API ADialogueTriggerActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADialogueTriggerActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void ClearWidget();
+
+public:
+	UPROPERTY(EditAnywhere)
+	float DisplayTime = 2.f;
+
+public:
+	UFUNCTION()
+	void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+
+public:
+	UPROPERTY(VisibleAnywhere)
+	class UBoxComponent* BoxComp;
+
+	UPROPERTY()
+	class UUserWidget* WidgetInstance;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UUserWidget> WidgetClass;
+
+	UPROPERTY(EditAnywhere)
+	USoundBase* OverlapSound;
+
+private:
+	FTimerHandle TimerHandle;
+};
