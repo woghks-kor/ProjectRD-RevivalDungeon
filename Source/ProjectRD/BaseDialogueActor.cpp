403a72130ac1836b726f911c3a21a9a2b9624c19
rudire28 <rudire@naver.com>
2025-08-04 10:00:47 +0900

    dvelop 통합

---
+++ Source/ProjectRD/BaseDialogueActor.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseDialogueActor.h"
+
+#include "DialogueComponent.h"
+
+
+// Sets default values
+ABaseDialogueActor::ABaseDialogueActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	DialogueComp = CreateDefaultSubobject<UDialogueComponent>(TEXT("DialogueComponent"));
+}
+
+// Called when the game starts or when spawned
+void ABaseDialogueActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseDialogueActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABaseDialogueActor::OnInteract()
+{
+	Super::OnInteract();
+
+	if (!bCanInteract) return;
+	if (!bCanPress) return;
+
+	DialogueComp->CreateDialogueWidget();
+	bCanPress = false;
+	CheckRepeat();
+}
+
---
+++ Source/ProjectRD/BaseDialogueActor.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseInteractableActor.h"
+#include "BaseDialogueActor.generated.h"
+
+UCLASS()
+class PROJECTRD_API ABaseDialogueActor : public ABaseInteractableActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseDialogueActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void OnInteract() override;
+
+public:
+	UPROPERTY(VisibleAnywhere)
+	class UDialogueComponent* DialogueComp;
+
+};
